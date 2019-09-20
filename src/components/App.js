63041aa080bb7c3382c57e120701e49b5abd8983
Aaron <aaronmassey45@gmail.com>
2019-09-20 12:29:02 -0500

    Ran prettier Use new App.scss file

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Button, Col, Grid, Row } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 
 import Card from './card';
 import Navbar from './navbar';
-import '../App.css';
+import '../App.scss';
 
 class App extends Component {
   constructor(props) {
@@ -12,21 +12,25 @@ class App extends Component {
     this.state = {
       deck_id: '',
       cards: '',
-      remaining: ''
-    }
+      remaining: '',
+    };
   }
 
   componentDidMount() {
-    axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+    axios
+      .get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
       .then(deck => {
-        axios.get(`https://deckofcardsapi.com/api/deck/${deck.data.deck_id}/draw/?count=2`)
+        axios
+          .get(
+            `https://deckofcardsapi.com/api/deck/${deck.data.deck_id}/draw/?count=2`
+          )
           .then(cards => {
             this.setState({
               deck_id: cards.data.deck_id,
               cards: cards.data.cards,
-              remaining: cards.data.remaining
-            })
-          })
+              remaining: cards.data.remaining,
+            });
+          });
       })
       .catch(err => {
         console.log(err);
@@ -34,25 +38,31 @@ class App extends Component {
   }
 
   drawAgain = () => {
-    this.setState({ cards: '' })
+    this.setState({ cards: '' });
     if (this.state.remaining === 0) {
-      axios.get(`https://cors-anywhere.herokuapp.com/https://deckofcardsapi.com/api/deck/${this.state.deck_id}/shuffle`)
-        .then((cards) => {
+      axios
+        .get(
+          `https://cors-anywhere.herokuapp.com/https://deckofcardsapi.com/api/deck/${this.state.deck_id}/shuffle`
+        )
+        .then(cards => {
           console.log(cards.data);
           this.drawAgain();
-          this.setState({ remaining: 52 })
-        })
+          this.setState({ remaining: 52 });
+        });
     } else {
-      axios.get(`https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/?count=2`)
+      axios
+        .get(
+          `https://deckofcardsapi.com/api/deck/${this.state.deck_id}/draw/?count=2`
+        )
         .then(cards => {
           console.log(cards.data.remaining);
           this.setState({
             cards: cards.data.cards,
-            remaining: cards.data.remaining
-          })
+            remaining: cards.data.remaining,
+          });
         });
     }
-  }
+  };
 
   getValue(card) {
     switch (card) {
@@ -63,9 +73,9 @@ class App extends Component {
       case 'KING':
         return 13;
       case 'ACE':
-        return Math.random() > .5 ? 14 : 1;
+        return Math.random() > 0.5 ? 14 : 1;
       default:
-        return card
+        return card;
     }
   }
 
@@ -75,14 +85,14 @@ class App extends Component {
     let winner;
 
     if (p1 > p2) {
-      winner = "Player 1 wins!"
+      winner = 'Player 1 wins!';
     } else if (p1 < p2) {
-      winner = "Player 2 wins!"
+      winner = 'Player 2 wins!';
     } else {
-      winner = "It's a tie!"
+      winner = "It's a tie!";
     }
     return winner;
-  }
+  };
 
   render() {
     let { cards } = this.state;
@@ -90,7 +100,7 @@ class App extends Component {
     if (!cards) {
       return (
         <div className="App">
-          <Navbar brand='High Card' />
+          <Navbar brand="High Card" />
           <h3>Ace can be high or low card!</h3>
           <p>Drawing cards...</p>
         </div>
@@ -102,23 +112,23 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Navbar brand='High Card' />
+        <Navbar brand="High Card" />
         <h3>Ace can be 1 or 14!</h3>
-        <Grid className='Cards'>
-          <Row>
-            <Col xs={6}>
-              <Card card={cards[0]} />
-              <p>{val0}</p>
-            </Col>
-            <Col xs={6}>
-              <Card card={cards[1]} />
-              <p>{val1}</p>
-            </Col>
-            <Col xs={12}>{this.winner(val0, val1)}</Col>
-          </Row>
-        </Grid>
+        <Row>
+          <Col xs={6}>
+            <Card card={cards[0]} />
+            <p>{val0}</p>
+          </Col>
+          <Col xs={6}>
+            <Card card={cards[1]} />
+            <p>{val1}</p>
+          </Col>
+          <Col xs={12}>{this.winner(val0, val1)}</Col>
+        </Row>
         <p>
-          <Button onClick={this.drawAgain} bsStyle="primary">Draw</Button>
+          <Button onClick={this.drawAgain} bsStyle="primary">
+            Draw
+          </Button>
         </p>
       </div>
     );
