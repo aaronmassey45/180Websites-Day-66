60e6b1303812b6bb1cd8ace2250eeb4e6721243b
Aaron <aaronmassey45@gmail.com>
2019-09-26 09:21:53 -0500

    Store deckid in ref instead of state

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 
@@ -8,24 +8,29 @@ import Navbar from './navbar';
 import '../App.scss';
 
 const App = () => {
-  const [deckId, setDeckId] = useState('');
   const [cards, setCards] = useState([]);
+  const deckId = useRef();
 
   useEffect(() => {
-    axios
-      .get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-      .then(response => {
-        setDeckId(response.data.deck_id);
-      });
-  }, []);
+    if (!deckId.current) {
+      axios
+        .get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then(response => {
+          deckId.current = response.data.deck_id;
+        })
+        .then(() => drawCards());
+    }
+  });
 
   async function drawCards() {
     const response = await axios.get(
-      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+      `https://deckofcardsapi.com/api/deck/${deckId.current}/draw/?count=2`
     );
 
     if (response.data.remaining === 0) {
-      await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
+      await axios.get(
+        `https://deckofcardsapi.com/api/deck/${deckId.current}/shuffle/`
+      );
       drawCards();
     }
 
