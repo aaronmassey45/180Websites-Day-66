ecae097dc2a8b4378594eefc798f7f0c61902135
Aaron <aaronmassey45@gmail.com>
2019-09-26 13:18:56 -0500

    Extracted deck logic to custom hook

---
+++ src/hooks/useDeck.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { useState, useRef } from 'react';
+
+const useDeck = () => {
+  const [cards, setCards] = useState([]);
+  const deckId = useRef(null);
+
+  const getTwoCards = async () => {
+    const response = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${deckId.current}/draw/?count=2`
+    );
+
+    if (response.data.remaining <= 10) {
+      axios.get(
+        `https://deckofcardsapi.com/api/deck/${deckId.current}/shuffle/`
+      );
+    }
+
+    setCards(response.data.cards);
+  };
+
+  const drawCards = () => {
+    if (!deckId.current) {
+      axios
+        .get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then(response => {
+          deckId.current = response.data.deck_id;
+        })
+        .then(() => getTwoCards());
+    } else {
+      getTwoCards();
+    }
+  };
+
+  return { cards, drawCards };
+};
+
+export default useDeck;
